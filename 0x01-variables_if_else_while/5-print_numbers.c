dffdc87a3b80e8ce29c665cf048479590fecc9d0
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-02-17 13:26:45 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - the starting point
+ * The programme prints number 0 to 9
+ * Return: 0
+ */
+int main(void)
+{
+	int f;
+
+	for (f = 0; f <= 10; f++)
+	{
+		putchar(f);
+	}
+	putchar('\n');
+	return (0);
+}
