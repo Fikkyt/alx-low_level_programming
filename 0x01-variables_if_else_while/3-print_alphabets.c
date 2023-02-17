090af2b445a38c939b4a93c94c69afb867fce318
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-02-17 13:07:09 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
-/** main - starting point
+/**
+ * main - starting point
  * The programme converts lowercase to uppercase
- * Return = 0
- */
+ * Return: 0
+*/
 int main(void)
 {
-	int k;
+	int k, m;
 
 	for (k = 'a'; k <= 'z'; k++)
 	{
 		putchar(k);
 	}
-	for (k = 'a'; k<= 'z'; k++)
+	for (m = 'A'; m <= 'Z'; m++)
 	{
-		putchar(toupper(k));
+		putchar(m);
 	}
 	putchar('\n');
 }
