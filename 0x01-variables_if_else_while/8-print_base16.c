efd03788c1ac25431f957bbf1314206a12dae1ce
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-02-17 15:08:03 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - the starting point
+ * The programme prints hexadecimal
+ * Return: 0
+*/
+int main(void)
+{
+	int m, k;
+
+	for (m == 0; m <= 9; m++)
+	{
+		putchar(m);
+	}
+	for (k == 'a'; k <= 'f'; k++)
+	{
+		putchar(k);
+	}
+	putchar('\n');
+	return (0);
+}
