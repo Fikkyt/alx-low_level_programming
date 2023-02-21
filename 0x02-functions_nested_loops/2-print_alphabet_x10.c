f7eb7e9c1d4ae5de8fb780614e60eebb5bbac7f0
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-02-21 14:33:20 -0800

    10 x alphabe

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
-* print_alphabets_x10 - prints the alphebets 10 times and include a new line
+* print_alphabet_x10 - entry point
+* Description: a function that prints the alphebets ten times
+* and also includes a new line
 * Return: void
 */
 void print_alphabet_x10(void)
 {
-	char m;
-	int n;
+	int m,  n;
 
-	for (n = '0'; n <= 9; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		for (m = 'a'; m <= 'z'; m++)
 		{
