af65c8c31bcc542f3962e7c9d57369fcc472aa04
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-02-21 15:47:58 -0800

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - the entry point
+ * Description: the function prints positive, zero or negative
+ * @n: the integer that the function receives
+ * Return: 1, 0 or -1
+ */
+int print_sign(int n)
+{
+	if (n > o)
+	{
+		_putchar('1');
+		return (1);
+	}}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-1');
+	}
+	return (-1);
+}
+
