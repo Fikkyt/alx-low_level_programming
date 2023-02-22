932db460143e9fd0b83ce52aadc487c164b105ae
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-02-22 08:11:12 -0800

    the 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,18 +16,23 @@ void times_table(void)
 			k = m * n;
 			tens = k / 10;
 			unit = k % 10;
-			if (k < 10)
+			if (n == 0)
 			{
-				_putchar(unit + '0');
+				_putchar('0');
+			}
+			else if (k < 10)
+			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(unit + '0');
 			}
 			else
 			{
-				_putchar(tens + '0');
-				_putchar(unit + '0');
 				_putchar(',');
 				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(unit + '0');
 			}
 		}
 		_putchar('\n');
