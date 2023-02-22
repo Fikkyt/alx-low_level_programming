4b8bcfd325a5d15e329f396082ff3822664eccc0
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-02-21 17:08:04 -0800

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - the entry point
+ * Description: functions that prints the last digit of a number
+ * @n: the placeholder
+ * Return: 0
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
