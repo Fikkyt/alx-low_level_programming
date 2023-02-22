6c42e3ee590c069cca76bc28606dc284f8ed4f42
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-02-22 08:55:29 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - the entry point
+ * Description: the functiin adds two integers
+ * @m: the integer 1
+ * @n: the integer 2
+ * Return: result
+ */
+int add(int m, int n)
+{
+	int result;
+
+	result = m + n;
+	return (result);
+}
