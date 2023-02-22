beb5a2e9c89a7f295e694a441d74c7df7743ad56
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-02-22 09:37:47 -0800

    98 Battery Street

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_to_98 - the entry point
+ * Description: thw function prints all natural numbers to 98
+ * @n: the integer n
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	printf("98\n");
+}
