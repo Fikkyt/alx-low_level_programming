7bec340baef5d3d690aceba4ff002f4ae2bc31d0
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-02-24 01:10:09 -0800

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - the entey point
+ * Description - the function prints 0 to 9 and insert a new line
+ * Return: void
+*/
+void print_numbers(void);
+{
+	int m;
+
+	if (m >= 48 && m <= 57)
+	{
+		_putchar('\n');
+		return (0);
+	}
+}
