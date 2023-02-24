4eaaa439328ea8eb69fb6631ffbe54fba76c8a62
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-02-24 08:49:07 -0800

    You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - the entry point
+ * Description: the function prints a square, followed by a new line
+ * @size: the integer
+ * Return: void
+*/
+void print_square(int size)
+{
+	int a;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
