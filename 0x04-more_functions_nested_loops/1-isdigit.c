5db93fc78e0603fe6fc44461dad797a4e50f8e13
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-02-24 00:47:10 -0800

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,7 @@
  * _isdigit - the main entry
  * Deacription: the function checks for a digit (0 through 9)
  * @c: the integer
- * Return: 1 if c or 0 otherwise 
+ * Return: 1 if c or 0 otherwise
 */
 int _isdigit(int c)
 {
