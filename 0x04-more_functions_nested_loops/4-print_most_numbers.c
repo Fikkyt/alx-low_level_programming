b68522a5d949f988b7b407362c3a67c97c5028ea
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-02-24 01:41:03 -0800

    I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_numbers - the entey point
+ * Description - the function prints 0 to 9 and insert a new line
+ * Return: void
+*/
+void print_most_numbers(void)
+{
+	char m = 0;
+
+	while (m <= 9)
+	{
+		if (m == 2 || m == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(m + '0');
+			m++;
+		}
+	}
+	_putchar('\n');
+}
