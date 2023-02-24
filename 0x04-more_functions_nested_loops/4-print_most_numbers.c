3bbf6be5688ec357114203ff3cd4ba15f4f62512
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-02-24 02:20:32 -0800

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_numbers - the entey point
+ * print_most_numbers - the entey point
  * Description - the function prints 0 to 9 and insert a new line
  * Return: void
 */
