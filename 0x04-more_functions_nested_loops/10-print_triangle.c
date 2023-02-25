ae35a8e890914ec5438463ad3ea68c9a55362446
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-02-24 16:07:30 -0800

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - the entry point
+ * Description: a function that prints a triangle followed by a new line
+ * @size: the integer
+ * Return: void
+*/
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (c = 1; c < size, c++)
+			{
+				_putchar(' ');
+			}
+			for (b = 0; b <= a; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+	}
+	_putchar('\n');
+}
