ebae403990babd5bdd862b3d74ef6f37773b7794
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-02-27 12:11:11 -0800

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen - the function that returns the length of a string
+ * @s: the char s
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	char str[];
+
+	*s = sizeof(str[]) - 1;
+}
