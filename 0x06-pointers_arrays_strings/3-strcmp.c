2deaed3d4cc56035886d778ef7818c9e299cfbc9
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-03-02 05:22:30 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,16 +7,20 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, sum_s1, sum_s2, diff;
+	int i, diff;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		sum_s1 += (int) (s1[i]);
+		if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+		{
+			diff = s1[i] - s2[i];
+		}
+		return (diff);
 	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		sum_s2 += (int)(s2[j]);
-	}
-	diff = sum_s1 - sum_s2;
-	return (diff);
 }
+
