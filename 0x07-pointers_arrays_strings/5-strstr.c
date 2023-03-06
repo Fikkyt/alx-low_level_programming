bf4d3bca665b1293629e1061029bbb95606ed37f
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-03-06 14:00:59 -0800

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strstr - the function that locates a substring
+ * @haystack: the destination string
+ * @needle: the source substrig
+ * Return: haystack
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (needle == '\0')
+		return (haystack);
+	for (i = 0; *haystack; i++)
+	{
+		for (i = 0; needle[i] >= '\0'; i++)
+		{
+			if (haystack[i] == needle[i])
+			{
+				haystack[i] = needle[i];
+			}
+		}
+	}
+	return (haystack + i);
+}
