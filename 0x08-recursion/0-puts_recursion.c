f859756fc291f1af49eb034b74d2d4a6d9e208c2
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-03-07 16:40:40 -0800

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,14 +6,14 @@
  */
 void _puts_recursion(char *s)
 {
-	if (s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
 	_putchar(*s);
 	s++;
-	if (s != '\0')
+	if (*s != '\0')
 	{
-		_puts_recursion(*s);
+		_puts_recursion(s);
 	}
 }
