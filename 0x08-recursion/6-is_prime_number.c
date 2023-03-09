02945d242bdc268d95622d075275846e78fcd456
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-03-08 18:29:48 -0800

    Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -25,13 +25,13 @@ int check_prime(int num, int div)
  */
 int is_prime_number(int n)
 {
-	int div == 2;
+	int div = 2;
 
 	if (n <= 1)
 	{
 		return (0);
 	}
-	if (n >= 3 && n <= 3)
+	if (n >= 2 && n <= 3)
 	{
 		return (1);
 	}
