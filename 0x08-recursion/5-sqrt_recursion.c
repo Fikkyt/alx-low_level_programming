0d701f115b415a4243fb64748f4e99826e6e763d
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-03-09 09:42:12 -0800

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,37 +1,39 @@
 #include "main.h"
 /**
- * check_sqrt - the function checks gthe square root of a number n
- * @num: the integer number to be find its square root
- * @y: update the multiplier of y by increment
- * Return: the sqrt
+ * root_checker - the function finds the root of a number
+ * @num: the required number
+ * @root: the expected root
+ * Return: 0
  */
-int check_sqrt(int num, int y)
+int root_checker(int num, int root)
 {
-	if (num == 0)
+	if (root * root == num)
 	{
-		return (0);
+		return (root);
 	}
-	if (num < 0)
+	if (root == num / 2)
 	{
 		return (-1);
 	}
-	return ((1 +  check_sqrt((num - 1) - (2 * (y + 1)))));
+	return (root_checker(num, root + 1));
 }
 /**
- * _sqrt_recursion - the function that returns
- * the natural square root of a number
- * @n: the number to find its square root
- * Return: the check_sqrt
+ * _sqrt_recursion - the function that returns the
+ * natural square root of a number.
+ * @n: the required number whose root is to be find
+ * Return: 0
  */
 int _sqrt_recursion(int n)
 {
-	int y = -1;
+	int root = 0;
 
-	if (n == 0)
-		return (0);
-	if (n == 1)
-		return (1);
 	if (n < 0)
+	{
 		return (-1);
-	return (check_sqrt(n, y));
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	return ((root_checker(n, root)));
 }
