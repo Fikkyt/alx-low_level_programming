4f0e9acbe67ab31fa24046ad3de9fe895d9feeea
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-03-10 05:49:53 -0800

    Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-gcc -c 0-isupper.c 0-memset.c 0-strcat.c 1-isdigit.c 1-memcpy.c 1-strncat.c
-gcc -c 100-atoi.c 2-strchr.c 2-strlen.c 2-strncpy.c 3-islower.c 3-puts.c
-gcc -c 3-strcmp.c 3-strspn.c 4-isalpha.c 4-strpbrk.c 5-strstr.c 6-abs.c
-gcc -c 9-strcpy.c _putchar.c
+gcc -c *.c
 ar -rcs liball.a *.o
