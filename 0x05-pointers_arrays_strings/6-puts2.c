2d725afafe910f39cc134cdb869ac810a2eef37f
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-03-11 14:23:50 -0800

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - the  function that prints every other character of a string,
+ * starting with the first character, followed by a new line
+ * @str: the pointer of character string
+ * Return: void
+ */
+void puts2(char *str)
+{
+	for (; *s != '\0'; )
+	{
+		-putchar(*s);
+		s += 2;
+	}
+	_putchar('\n');
+}
