da5dd84e91b5bb40f9851bf5303a0bb8d06a8d11
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2023-03-11 11:41:15 -0800

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - the function that prints a string, followed by
+ *  a new line, to stdou
+ *  @str: the pointer to string characters
+ *  Return: 0
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; s++)
+	{
+		_putchar(*s);
+	}
+	_putchar('\n');
+}
